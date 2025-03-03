937757b5e286d862ad008c6ce9ac6b1fd2d3539b
helmisaid <saidkun219@gmail.com>
2025-03-04 06:29:10 +0700

    feat(SEO): Add robots.txt and sitemap configuration

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://helmisaid.site',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 1,
+      },
+    ]
+  }
